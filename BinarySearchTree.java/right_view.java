a040b4420af74748addd771eaedae10a4a9d6166
shubham81368 <shubham81368@gmail.com>
2024-08-26 21:24:34 +0530

    Right_view_of a bianary tree

---
+++ BinarySearchTree.java/right_view.java
@@ -0,0 +1,93 @@
+
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+
+class Main {
+    static Node buildTree(String str){
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        String ip[] = str.split(" ");
+        Node root = new Node(Integer.parseInt(ip[0]));
+        Queue<Node> queue = new LinkedList<>(); 
+        queue.add(root);
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            Node currNode = queue.peek();
+            queue.remove();
+            String currVal = ip[i];
+            if(!currVal.equals("N")) {
+                currNode.left = new Node(Integer.parseInt(currVal));
+                queue.add(currNode.left);
+            }
+            i++;
+            if(i >= ip.length)
+                break;   
+            currVal = ip[i];
+            if(!currVal.equals("N")) {
+                currNode.right = new Node(Integer.parseInt(currVal));
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        return root;
+    }
+    void inOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+        inOrder(node.left);
+        System.out.print(node.data + " ");
+        inOrder(node.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t=Integer.parseInt(br.readLine());
+		while(t-- > 0){
+			String s = br.readLine();
+			Node root = buildTree(s);
+			Solution tree = new Solution();
+			ArrayList<Integer> arr = tree.rightView(root);
+			for(int x : arr)
+			System.out.print(x +" ");
+			System.out.println();  
+		}
+	}
+}
+  
+
+
+class Solution{//t.c=o(n) ->if tree is squed the it travels n node 
+// s.c=O(h)->height of the binary tree
+    ArrayList<Integer> rightView(Node root) {
+	
+    ArrayList<Integer>ans=new ArrayList<>();
+
+    helper(root,0,ans);
+    return ans;
+	
+    }
+
+    public void helper(Node root,int l,ArrayList<Integer>ans){
+        if(root==null)return;
+        if(l==ans.size())ans.add(root.data);
+        helper(root.right,l+1,ans);
+        helper(root.left,l+1,ans);
+    }
+}
+
