bb2781823f1898490edf8d3c67912de9c87c826c
shubham81368 <shubham81368@gmail.com>
2024-09-17 17:44:45 +0530

    Minimise height ||

---
+++ Array/Minimise the height.java
@@ -0,0 +1,19 @@
+class Solution {
+    int getMinDiff(int[] arr, int k) {
+        // code here
+        int min=0,max=0,r=0;
+        int n=arr.length;
+        Arrays.sort(arr);
+        r=arr[n-1]-arr[0];
+        for(int i=1;i<n;i++){
+            if(arr[i]>=k){
+            max=Math.max(arr[i-1]+k,arr[n-1]-k);
+            min=Math.min(arr[i]-k,arr[0]+k);
+            r=Math.min(r,max-min);
+        }else{
+            continue;
+        }
+        }
+        return r;
+    }
+}
