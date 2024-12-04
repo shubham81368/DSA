300c6d3b41e79cd97aec8b44e5bdd5cdf81110c2
shubham81368 <shubham81368@gmail.com>
2024-12-04 10:26:59 +0530

    String rotation of each other

---
+++ String/String rotation each other.java
@@ -0,0 +1,11 @@
+    public static boolean areRotations(String s1, String s2) {
+        // Your code here
+    
+        
+        
+       String s=s1+s1;
+       
+
+        
+       return s.lastIndexOf(s2)>=0;
+    }
