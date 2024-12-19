3bc47a7b1b8f0c0ca0e91a933e84ddec2f60052a
shubham81368 <shubham81368@gmail.com>
2024-12-19 21:32:16 +0530

    kth missing number

---
+++ BinarySearchTree.java/kth missing number.java
@@ -0,0 +1,15 @@
+
+
+class Solution {
+    public int kthMissing(int[] arr, int k) {
+        // code here
+       for(int i=0;i<arr.length;i++){
+           if(arr[i]<=k){
+               k++;
+           }else{
+               break;
+           }
+       }
+       return k;
+    }
+}
