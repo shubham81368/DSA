6a577aa40314f53c4b0325f300c404c13ea1a737
shubham81368 <shubham81368@gmail.com>
2025-02-10 22:31:39 +0530

    Clear digits

---
+++ String/Clear digits.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String clearDigits(String s) {
+        StringBuilder sb=new StringBuilder();
+        for(char c:s.toCharArray()){
+            if(c>='0'&&c<='9'){
+                if(sb.length()>0)sb.deleteCharAt(sb.length()-1);
+            }
+            else{
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+}
