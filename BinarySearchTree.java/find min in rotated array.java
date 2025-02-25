2092f89806f472c9e80ac346771fa233ef49736c
shubham81368 <shubham81368@gmail.com>
2025-02-25 22:01:45 +0530

    find minimum in rotated array

---
+++ BinarySearchTree.java/find min in rotated array.java
@@ -0,0 +1,42 @@
+// Java program to find minimum element in a sorted and
+// rotated array
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+class Solution
+{
+    static int findMin(int arr[], int low, int high)
+    {
+        //Write your code here
+    
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(arr[mid]<arr[mid-1]){
+                return arr[mid];
+            }
+            else if(arr[0]<=arr[mid]){
+            
+                low=mid+1;
+                
+            }
+            else{
+                high=mid-1;;
+            }
+        }
+        return arr[low];
+    }
+}
+class Main {
+    
+ 
+    // Driver Program
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0; i<n; i++)
+        arr[i] = sc.nextInt();
+        System.out.println(Solution.findMin(arr, 0, n - 1));
+    }
+}
