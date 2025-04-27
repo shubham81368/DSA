3576617745814692771c3565e65a650e1d6d4f62
shubham81368 <shubham81368@gmail.com>
2025-04-27 23:27:08 +0530

    count subarrays of length 3 with condition

---
+++ Array/count subarrays of length 3.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int countSubarrays(int[] nums) {
+        int cnt=0;
+        int n=nums.length;
+       if(n<3)return 0;
+       for(int i=0;i<n-2;i++){
+        if(nums[i]+nums[i+2]==nums[i+1]/2.0)cnt++;
+       }
+       return cnt;
+    }
+}
